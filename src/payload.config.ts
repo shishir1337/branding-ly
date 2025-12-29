d29a2d72bbf4345c88e7699b9bdbc2f3bf8237f8
shishirahmed1337 <shishir.ahmed@dizishore.com>
2025-12-30 00:12:10 +0600

    Revert "Fix: host"
    This reverts commit e9dc684d7eeef717a43dc23d5d3924513fb76cca.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -60,7 +60,7 @@ export default buildConfig({
   editor: defaultLexical,
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || process.env.DATABASE_URL || '',
+      connectionString: process.env.DATABASE_URI || '',
     },
   }),
   collections: [Pages, Posts, Media, Categories, Services, Users],
